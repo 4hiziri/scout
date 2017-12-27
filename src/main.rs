4c09cebb90ae7cbb3dcfd2c5a079008e2060ba1d
tkgsy <meirvg@gmail.com>
2017-12-27 23:58:00 +0900

    Add get_store_path.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use clap::App;
 use rustc_serialize::json;
 use std::path::PathBuf;
 use std::env;
+use std::fs::File;
 
 /// scout - doc management tool
 /// # goal
@@ -32,8 +33,30 @@ fn get_env_var() -> Option<PathBuf> {
     }
 }
 
-fn add_path(path: String, tags: Vec<String>) {
-    let path = get_env_var().unwrap_or(env::home_dir().unwrap());
+fn get_store_path() -> PathBuf {
+    match get_env_var() {
+        Some(path) => path,
+        None => {
+            let mut tmp = env::home_dir().unwrap();
+            tmp.push(".scout");
+            tmp
+        }
+    }
+}
+
+fn add_path(file_path: String, tags: Vec<String>) {
+    let mut path_entries: Vec<PathEntry> = Vec::new();
+    let mut store_path = get_store_path();
+    store_path.push("pathes.json");
+    let store_path = store_path;
+    println!("{:?}", &store_path);
+    // FIX: if .scout doesn't exist, create_dirx
+    let mut pathes_file = if !store_path.exists() {
+        // TODO: error handling
+        File::create(store_path).unwrap()
+    } else {
+        File::open(store_path).unwrap()
+    };
 }
 
 fn main() {
@@ -50,11 +73,15 @@ fn main() {
     if let Some(matches) = matches.subcommand_matches("add") {
         let input = matches.value_of("PATH").unwrap();
 
+        // TODO: check whether file exists or not.
         println!("input is {}", input);
 
+        // TODO: tag parser
         if matches.is_present("tags") {
             let tags = matches.value_of("tags").unwrap_or("nothing");
             println!("tags is {}", tags);
         }
+
+        add_path(input.to_string(), Vec::new());
     }
 }
